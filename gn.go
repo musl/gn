f6b6873cf3338ecc61b69b25fe4a413ace9e3321
Mike Hix <m@hix.io>
2015-08-14 16:20:12 -0700

    Initial FFT filter take, basically a re-write.

--- gn.go
+++ gn.go
@@ -3,8 +3,12 @@ package main
 
 import (
 "code.google.com/p/portaudio-go/portaudio"
-"fmt"
+"github.com/mjibson/go-dsp/fft"
+//"github.com/mjibson/go-dsp/window"
 "flag"
+"fmt"
+"math"
+//"math/cmplx"
 "math/rand"
 "os"
 "syscall"
@@ -12,61 +16,150 @@ import (
 )
 
 type Config struct {
-	Vol  float64
-	//Var  float64
-	//Freq float64
-	Time int64
+	Vol        float64
+	Time       int64
+	SampleRate float64
+	Quiet      bool
+	FrameSize  int
 }
 
-func main() {
-	config := Config{}
+type Frame struct {
+	Left  []float64
+	Right []float64
+}
 
-	flag.Float64Var(&config.Vol, "volume", 0.5, "output volume")
-	//flag.Float64Var(&config.Var, "amplitude", 0.1, "amplitude variance")
-	//flag.Float64Var(&config.Freq, "rate", 0.1, "rate of variance")
-	flag.Int64Var(&config.Time, "time", 3600, "length of play")
-	flag.Parse();
+func (self *Frame) Fill() {
+	for i := range self.Left {
+		self.Left[i] = rand.Float64();
+		self.Right[i] = rand.Float64();
+	}
+}
 
-	fmt.Println("gn v0.0.0")
+func (self *Frame) Filter(config *Config) {
+	i := 0
+	//p := math.Pi * 0.0625
+	p := 0.0
+	t := 0.0
+	s := 0.1 / config.SampleRate
+	m := 0.0
+	n := 0.0001
+	v := 0.0
+	x := 0.0
 
-	// Shut. Up.
-	dev_null, _ := os.Open("/dev/null")
-	syscall.Dup2(int(dev_null.Fd()), 1)
-   	syscall.Dup2(int(dev_null.Fd()), 2)
+	l := fft.FFTReal(self.Left)
+	r := fft.FFTReal(self.Right)
 
-	play(&config);
+	m = float64(len(l))
+	for i = range l {
+		p = 0.5 * math.Sin(2.0 * math.Pi * t)
+		_, t = math.Modf(t + s)
+		x = float64(i)
+		v = 0.5 + 0.5 * math.Cos(p + ((1.0 + n) / ((n * m / x) * m + x)) * math.Pi * x)
+		self.Left[i] = real(l[i]) * v
+		self.Right[i] = real(r[i]) * v
+	}
+
+	l = fft.IFFTReal(self.Left)
+	r = fft.IFFTReal(self.Right)
+
+	for i := range self.Left {
+		self.Left[i] = real(l[i]) * config.Vol
+		self.Right[i] = real(r[i]) * config.Vol
+	}
+}
+
+func (self *Frame) DePop(last *Frame) {
+	n := 8
+	l := last.Left[n - 1]
+	r := last.Right[n - 1]
+	a := 0.0
+	b := 0.0
+
+	for i := 0; i < n; i ++ {
+		a = float64(i) / float64(n)
+		b = 1.0 - a
+		self.Left[i] = self.Left[i] * a + l * b
+		self.Right[i] = self.Right[i] * a + r * b
+	}
+}
+
+func (self *Frame) Copy(out []float32) {
+	for i := range self.Left {
+		out[i * 2] = float32(self.Left[i])
+		out[i * 2 + 1] = float32(self.Right[i])
+	}
 }
 
 func play(config *Config) {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
-	h, err := portaudio.DefaultHostApi()
-	chk(err)
+	frames := make(chan Frame, 4)
+	last := make_frame(config)
 
-	device := h.DefaultOutputDevice
-	params := portaudio.HighLatencyParameters(nil, device)
-	callback := func(out []int32) {
-		for i := range out {
-			out[i] = int32(float64(rand.Uint32()) * config.Vol)
+	produce := func() {
+		for {
+			frames<- make_frame(config)
 		}
-
-		// TODO: Apply a slowly shifting EQ to the block of samples.
-
 	}
 
-	stream, err := portaudio.OpenStream(params, callback)
-	chk(err)
+	consume := func(out []float32) {
+		frame := <-frames
+		frame.DePop(&last)
+		frame.Copy(out)
+		last = frame
+	}
 
+	stream, err := portaudio.OpenDefaultStream(0, 2, config.SampleRate, config.FrameSize, consume)
+	check_error(err)
+
+	check_error(stream.Start())
 	defer stream.Close()
-	chk(stream.Start())
+
+	// pre-fill the buffer
+	for i := 0; i < len(frames); i++ {
+		frames<- make_frame(config)
+	}
+
+	go produce()
 
 	time.Sleep(time.Duration(config.Time) * time.Second)
-	chk(stream.Stop())
+	check_error(stream.Stop())
 }
 
-func chk(err error) {
+func make_frame(config *Config) Frame {
+	f := Frame{}
+	f.Left = make([]float64, config.FrameSize)
+	f.Right = make([]float64, config.FrameSize)
+	f.Fill()
+	f.Filter(config)
+	return f
+}
+
+func check_error(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
+
+func main() {
+	config := Config{}
+
+	flag.Float64Var(&config.Vol, "volume", 0.5, "output volume as a float")
+	flag.Int64Var(&config.Time, "time", 3600, "length of play in seconds")
+	flag.IntVar(&config.FrameSize, "frame-size", 131072, "frame size")
+	flag.Float64Var(&config.SampleRate, "sample-rate", 44100, "samples per second")
+	flag.BoolVar(&config.Quiet, "quiet", true, "Don't print messages.")
+	flag.Parse()
+
+	if config.Quiet {
+		// Shut. Up.
+		dev_null, _ := os.Open("/dev/null")
+		syscall.Dup2(int(dev_null.Fd()), 1)
+		syscall.Dup2(int(dev_null.Fd()), 2)
+	}
+
+	fmt.Println("gn v0.0.0")
+	play(&config);
+}
+
