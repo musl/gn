f75ed6799bebaa1f9729c870f0e2d0ad8c643f71
Mike Hix <m@hix.io>
2017-11-03 23:54:31 -0700

    Make everything configurable, parallelize some loops

--- cmd/gn/gn.go
+++ cmd/gn/gn.go
@@ -11,6 +11,7 @@ import (
 	"math/rand"
 	"os"
 	"runtime"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -22,10 +23,16 @@ func check_error(err error) {
 }
 
 type Config struct {
-	Quiet      bool
-	SampleRate int
-	Time       int
-	Vol        float64
+	Quiet        bool
+	SampleRate   int
+	Time         int
+	Vol          float64
+	BufferCount  int
+	SegmentSize  int
+	SegmentCount int
+	Alpha        float64
+	Gain         float64
+	Cutoff       int
 }
 
 type Buffer struct {
@@ -116,6 +123,7 @@ func (self *Buffer) Copy(out []float32) {
 }
 
 func (self Filter) Apply(in, out *Buffer) {
+	var wg sync.WaitGroup
 	win := window.Hann
 	size := in.SegmentSize
 	half := size / 2
@@ -140,15 +148,25 @@ func (self Filter) Apply(in, out *Buffer) {
 	/*
 	 * Apply Windowing Functions
 	 */
-	for i := 0; i < len(la); i += size {
-		window.Apply(la[i:i+size], win)
-		window.Apply(ra[i:i+size], win)
-	}
+	wg.Add(2)
 
-	for i := 0; i < len(lb); i += size {
-		window.Apply(lb[i:i+size], win)
-		window.Apply(rb[i:i+size], win)
-	}
+	go func() {
+		defer wg.Done()
+		for i := 0; i < len(la); i += size {
+			window.Apply(la[i:i+size], win)
+			window.Apply(ra[i:i+size], win)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < len(lb); i += size {
+			window.Apply(lb[i:i+size], win)
+			window.Apply(rb[i:i+size], win)
+		}
+	}()
+
+	wg.Wait()
 
 	/*
 	 * Convert Real to Complex Numbers
@@ -161,49 +179,59 @@ func (self Filter) Apply(in, out *Buffer) {
 	/*
 	 * Apply FFT, Filter, & IFFT
 	 */
-	for i := 0; i < len(la); i += size {
-		ls := lc[i : i+size]
-		rs := rc[i : i+size]
-		ls = fft.FFT(ls)
-		rs = fft.FFT(rs)
+	wg.Add(2)
 
-		for j := range ls {
-			f := 0.0
-			// Something something DC and Nyquist bins...
-			if j > self.Cutoff && j != size/2 {
-				f = 1.0 / (self.Alpha*float64(j) + 1.0)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < len(la); i += size {
+			ls := lc[i : i+size]
+			rs := rc[i : i+size]
+			ls = fft.FFT(ls)
+			rs = fft.FFT(rs)
+
+			for j := range ls {
+				f := 0.0
+				// Something something DC and Nyquist bins...
+				if j > self.Cutoff && j != size/2 {
+					f = 1.0 / (self.Alpha*float64(j) + 1.0)
+				}
+				ls[j] *= complex(f, 0.0)
+				rs[j] *= complex(f, 0.0)
 			}
-			ls[j] *= complex(f, 0.0)
-			rs[j] *= complex(f, 0.0)
+
+			ls = fft.IFFT(ls)
+			rs = fft.IFFT(rs)
+			copy(lc[i:i+size], ls)
+			copy(rc[i:i+size], rs)
 		}
+	}()
 
-		ls = fft.IFFT(ls)
-		rs = fft.IFFT(rs)
-		copy(lc[i:i+size], ls)
-		copy(rc[i:i+size], rs)
-	}
+	go func() {
+		defer wg.Done()
+		for i := 0; i < len(lb); i += size {
+			ls := ld[i : i+size]
+			rs := rd[i : i+size]
+			ls = fft.FFT(ls)
+			rs = fft.FFT(rs)
 
-	for i := 0; i < len(lb); i += size {
-		ls := ld[i : i+size]
-		rs := rd[i : i+size]
-		ls = fft.FFT(ls)
-		rs = fft.FFT(rs)
-
-		for j := range ls {
-			f := 0.0
-			// Something something DC and Nyquist bins...
-			if j > self.Cutoff && j != size/2 {
-				f = 1.0 / (self.Alpha*float64(j) + 1.0)
+			for j := range ls {
+				f := 0.0
+				// Something something DC and Nyquist bins...
+				if j > self.Cutoff && j != size/2 {
+					f = 1.0 / (self.Alpha*float64(j) + 1.0)
+				}
+				ls[j] *= complex(f, 0.0)
+				rs[j] *= complex(f, 0.0)
 			}
-			ls[j] *= complex(f, 0.0)
-			rs[j] *= complex(f, 0.0)
-		}
 
-		ls = fft.IFFT(ls)
-		rs = fft.IFFT(rs)
-		copy(ld[i:i+size], ls)
-		copy(rd[i:i+size], rs)
-	}
+			ls = fft.IFFT(ls)
+			rs = fft.IFFT(rs)
+			copy(ld[i:i+size], ls)
+			copy(rd[i:i+size], rs)
+		}
+	}()
+
+	wg.Wait()
 
 	/*
 	 * Add Buffers
@@ -221,38 +249,46 @@ func (self Filter) Apply(in, out *Buffer) {
 	*/
 }
 
+func produce(buffers chan Buffer, config *Config) {
+	noise := NewBuffer(config.SegmentCount+1, config.SegmentSize)
+	filtered := NewBuffer(config.SegmentCount, config.SegmentSize)
+	filter := Filter{
+		Alpha:  config.Alpha,
+		Cutoff: config.Cutoff,
+		Gain:   config.Gain,
+	}
+
+	noise.Fill()
+
+	for {
+		noise.ShiftAndFill()
+		filter.Apply(&noise, &filtered)
+		filtered.Mul(config.Vol)
+		buffers <- filtered
+	}
+}
+
+func consume(buffers chan Buffer) func([]float32) {
+	return func(out []float32) {
+		buf := <-buffers
+		buf.Copy(out)
+	}
+}
+
 func play(config *Config) {
-	rand.Seed(time.Now().UTC().UnixNano())
+	buffers := make(chan Buffer, config.BufferCount)
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
-	buffers := make(chan Buffer, 3)
-	go func() {
-		noise := NewBuffer(11, 16384)
-		filtered := NewBuffer(10, 16384)
-		filter := Filter{
-			Alpha:  0.1,
-			Cutoff: 5,
-			Gain:   10.0,
-		}
-
-		noise.Fill()
-		for {
-			noise.ShiftAndFill()
-			filter.Apply(&noise, &filtered)
-			filtered.Mul(config.Vol)
-			buffers <- filtered
-		}
-	}()
-
-	consume := func(out []float32) {
-		buf := <-buffers
-		buf.Copy(out)
-	}
+	go produce(buffers, config)
 
 	stream, err := portaudio.OpenDefaultStream(
-		0, 2, float64(config.SampleRate), 163840, consume)
+		0,
+		2,
+		float64(config.SampleRate),
+		config.SegmentSize*config.SegmentCount,
+		consume(buffers))
 	check_error(err)
 
 	check_error(stream.Start())
@@ -268,8 +304,14 @@ func main() {
 
 	flag.BoolVar(&config.Quiet, "quiet", true, "Don't print messages.")
 	flag.IntVar(&config.SampleRate, "sample-rate", 44100, "samples per second")
+	flag.IntVar(&config.BufferCount, "buffer-count", 3, "number of buffers to calculate ahead of time")
+	flag.IntVar(&config.SegmentCount, "segment-count", 10, "segments per buffer")
+	flag.IntVar(&config.SegmentSize, "segment-size", 16384, "buffer segment size in samples")
 	flag.IntVar(&config.Time, "time", 3600, "length of play in seconds")
 	flag.Float64Var(&config.Vol, "volume", 1.0, "output volume as a float")
+	flag.Float64Var(&config.Alpha, "alpha", 0.1, "0.0 is white noise, 0.1 is pink-ish noise")
+	flag.IntVar(&config.Cutoff, "cutoff", 5, "discard fft buckets starting at 0")
+	flag.Float64Var(&config.Gain, "gain", 10.0, "use this to adjust for low alpha values")
 	flag.Parse()
 
 	if config.Quiet {
@@ -279,6 +321,7 @@ func main() {
 	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	rand.Seed(time.Now().UTC().UnixNano())
 
 	fmt.Println("gn v0.0.0")
 	play(&config)
